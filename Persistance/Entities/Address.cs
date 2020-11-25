e15c4556ee670f290b78b1633a1eb8a37c17fc47
Łukasz Mojek <lukasz.mojek@gmail.com>
2020-11-25 23:34:50 +0100

    - Created projects to separate persistance, infarastructure and business logic - First migration added

---
+++ Persistance/Entities/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace Persistance.Entities
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string Street { get; set; }
+        public string HouseAndFlatNumber { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
