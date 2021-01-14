0f430952df758365f330e6c9b752412509fca086
lukaszmojek <lukasz.mojek@gmail.com>
2021-01-14 22:31:02 +0100

    Modified PaymentDto's

--- Contracts/Requests/RegisterPackageDto.cs
+++ Contracts/Requests/RegisterPackageDto.cs
@@ -6,6 +6,6 @@ namespace Contracts.Requests
         public AddressDto ReceiveAddress { get; set; }
         public PackageDetailsDto Package { get; set; }
         public int ServiceId { get; set; }
-        public PaymentDto Payment { get; set; }
+        public CreatePaymentDto Payment { get; set; }
     }
 }
