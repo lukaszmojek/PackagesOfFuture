6596db5ce7cac813021693a221e9a7b86ce023a1
lukaszmojek <lukasz.mojek@gmail.com>
2021-10-10 13:59:40 +0200

    Add main app component with concept of a layout for the pages available after logging in

--- PackagesOfFuture.WebUI/src/app/shared/material.module.ts
+++ PackagesOfFuture.WebUI/src/app/shared/material.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core'
 import { MatSliderModule } from '@angular/material/slider'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
 
-const materialModules = [MatSliderModule]
+const materialModules = [
+  MatSliderModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatButtonModule
+]
 
 @NgModule({
   imports: [...materialModules],
