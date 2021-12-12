aa2fa2d46476ae8be0e7ddc28fdd80595610e10c
lukaszmojek <lukasz.mojek@gmail.com>
2021-12-12 21:48:41 +0100

    Extend claims in JWT by user type

--- PackagesOfFuture.Backend/Api/Services/UserService.cs
+++ PackagesOfFuture.Backend/Api/Services/UserService.cs
@@ -26,14 +26,24 @@ namespace Api.Services
 
         private string GenerateJwtToken(User user)
         {
+            // var claimsAndRoles = new Dictionary<string, object>();
+            // claimsAndRoles.Add("claims", new Dictionary<string, string>{{"hasAccessToManager", "no"}});
             var tokenHandler = new JwtSecurityTokenHandler();
             //TODO: Move that 'secret' to appsettings and change it
             var key = Encoding.ASCII.GetBytes("dupa123asdfasdgasdffasdasdfghasdfasdfasdwfawefasdv");//_appSettings.Secret);
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("id", user.Id.ToString()), 
+                    new Claim("role", user.Type.ToString())
+                }),
+                // Claims = claimsAndRoles,
                 Expires = DateTime.UtcNow.AddHours(1),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                    )
             };
             
             var token = tokenHandler.CreateToken(tokenDescriptor);
