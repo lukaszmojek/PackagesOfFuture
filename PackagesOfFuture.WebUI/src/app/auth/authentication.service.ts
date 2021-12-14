b9922aeb3f0fe4a31561b0f82622a0556ba7909a
lukaszmojek <lukasz.mojek@gmail.com>
2021-12-15 00:35:41 +0100

    Add storing token in local storage

--- PackagesOfFuture.WebUI/src/app/auth/authentication.service.ts
+++ PackagesOfFuture.WebUI/src/app/auth/authentication.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AppSettings } from '../common/appsettings';
 import { IApiResponse, ITokenResponse } from './models';
 
@@ -8,6 +9,12 @@ import { IApiResponse, ITokenResponse } from './models';
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private readonly localStorageTokenKey = 'pof_token_key'
+
+  public get tokenEntryExistsInLocalStorage(): boolean {
+    return !!localStorage.getItem(this.localStorageTokenKey)
+  }
+
   constructor(private http: HttpClient) { }
 
   public logIn$(email: string, password: string): Observable<IApiResponse<ITokenResponse>> {
@@ -16,10 +23,16 @@ export class AuthenticationService {
       password: password
     }
 
-    return this.http.post<IApiResponse<ITokenResponse>>(AppSettings.authEndpoint, request)
+    return this.http.post<IApiResponse<ITokenResponse>>(AppSettings.authEndpoint, request).pipe(
+      tap(tokenResponse => {
+        localStorage.setItem(this.localStorageTokenKey, tokenResponse.content.token)
+      })
+    )
   }
 
   public logOut$(): Observable<any> {
+    localStorage.removeItem(this.localStorageTokenKey)
+    
     //TODO: Think how to change that to different observable,
     //TODO: there might be something more appropiate to use here
     return of('')
