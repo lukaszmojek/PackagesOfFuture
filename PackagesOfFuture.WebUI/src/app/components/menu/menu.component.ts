f52a731fd56ddf493f22eebbe79b5d0ff3f712ae
lukaszmojek <lukasz.mojek@gmail.com>
2022-01-13 00:26:22 +0100

    Update menu items

--- PackagesOfFuture.WebUI/src/app/components/menu/menu.component.ts
+++ PackagesOfFuture.WebUI/src/app/components/menu/menu.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthorizationService } from 'src/app/auth/authorization.service';
 
 @Component({
   selector: 'pof-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.sass']
 })
-export class MenuComponent implements OnInit {
-
-  
-  constructor() { }
-
-  ngOnInit(): void {
+export class MenuComponent {
+  public get userId(): number {
+    return this.auth.currentUserId()
   }
 
+  constructor(private auth: AuthorizationService) { }
 }
