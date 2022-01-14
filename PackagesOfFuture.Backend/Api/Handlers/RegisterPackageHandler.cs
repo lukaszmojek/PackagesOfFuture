12b128b2ce4c78fd1b51aba314c20c33eb901024
dblaszkiewicz <daw.blaszkiewicz@gmail.com>
2022-01-14 18:58:49 +0100

    transakcja w dodawaniu paczki

--- PackagesOfFuture.Backend/Api/Handlers/RegisterPackageHandler.cs
+++ PackagesOfFuture.Backend/Api/Handlers/RegisterPackageHandler.cs
@@ -10,7 +10,9 @@ using Contracts.Responses;
 using Data.Entities;
 using Infrastructure.Repositories;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using ResourceEnums;
+using System.Data;
 
 namespace Api.Handlers
 {
@@ -22,14 +24,15 @@ namespace Api.Handlers
         private readonly IRepository<Payment> _paymentRepository;
         private readonly IRepository<Sorting> _sortingRepository;
         private readonly IMapper _mapper;
+        private readonly DbContext _dbContext;
 
         public RegisterPackageHandler(
-            IRepository<Package> packageRepository, 
-            IRepository<Address> addressRepository, 
-            IRepository<Payment> paymentRepository, 
-            IRepository<Service> serviceRepository, 
+            IRepository<Package> packageRepository,
+            IRepository<Address> addressRepository,
+            IRepository<Payment> paymentRepository,
+            IRepository<Service> serviceRepository,
             IRepository<Sorting> sortingRepository,
-            IMapper mapper)
+            IMapper mapper, DbContext dbContext)
         {
             _packageRepository = packageRepository;
             _addressRepository = addressRepository;
@@ -38,63 +41,82 @@ namespace Api.Handlers
             _serviceRepository = serviceRepository;
             _sortingRepository = sortingRepository;
             _mapper = mapper;
+            _dbContext = dbContext;
         }
 
         public async Task<Response<RegisterPackageResponse>> Handle(RegisterPackageCommand request, CancellationToken cancellationToken)
         {
-            var addresses = await _addressRepository.GetAsync();
-            var sortings = await _sortingRepository.GetAsync();
-
-            var pickupAddress = addresses.FirstOrDefault(x => AddressesAreEqual(x, request.ReceiveAddress));
-            var destinationAddress = addresses.FirstOrDefault(x => AddressesAreEqual(x, request.DeliveryAddress));
-            var defaultSorting = sortings.First();
-
-            var package = _mapper.Map<Package>(request.Package);
-
-            if (pickupAddress == null)
+            using (var transaction = _dbContext.Database.BeginTransaction(IsolationLevel.Serializable))
             {
-                var pickupAddressEntity = _mapper.Map<Address>(request.ReceiveAddress);
-                await _addressRepository.AddAsync(pickupAddressEntity);
-                package.ReceiveAddress = pickupAddressEntity;
-            }
-            else
-            {
-                package.ReceiveAddressId = pickupAddress.Id;
-            }
-            
-            if (destinationAddress == null)
-            {
-                var deliveryAddressEntity = _mapper.Map<Address>(request.DeliveryAddress);
-                await _addressRepository.AddAsync(deliveryAddressEntity);
-                package.DeliveryAddress = deliveryAddressEntity;
-            }
-            else
-            {
-                package.DeliveryAddressId = destinationAddress.Id;
-            }
+                try
+                {
+                    var addresses = await _addressRepository.GetAsync();
+                    var sortings = await _sortingRepository.GetAsync();
 
-            var service = await _serviceRepository.GetByIdAsync(request.ServiceId);
-            package.Service = service;
+                    var pickupAddress = addresses.FirstOrDefault(x => AddressesAreEqual(x, request.ReceiveAddress));
+                    var destinationAddress = addresses.FirstOrDefault(x => AddressesAreEqual(x, request.DeliveryAddress));
+                    var defaultSorting = sortings.FirstOrDefault();
 
-            if (service == null)
-            {
-                throw new ArgumentException(nameof(request.ServiceId));
+                    var package = _mapper.Map<Package>(request.Package);
+
+                    if (pickupAddress == null)
+                    {
+                        var pickupAddressEntity = _mapper.Map<Address>(request.ReceiveAddress);
+                        await _addressRepository.AddAsync(pickupAddressEntity);
+                        package.ReceiveAddress = pickupAddressEntity;
+                    }
+                    else
+                    {
+                        package.ReceiveAddressId = pickupAddress.Id;
+                    }
+
+                    if (destinationAddress == null)
+                    {
+                        var deliveryAddressEntity = _mapper.Map<Address>(request.DeliveryAddress);
+                        await _addressRepository.AddAsync(deliveryAddressEntity);
+                        package.DeliveryAddress = deliveryAddressEntity;
+                    }
+                    else
+                    {
+                        package.DeliveryAddressId = destinationAddress.Id;
+                    }
+
+                    var service = await _serviceRepository.GetByIdAsync(request.ServiceId);
+                    package.Service = service;
+
+                    if (service == null)
+                    {
+                        throw new ArgumentException(nameof(request.ServiceId));
+                    }
+
+                    var payment = _mapper.Map<Payment>(request.Payment);
+                    payment.Status = PaymentStatus.InProgress;
+                    await _paymentRepository.AddAsync(payment);
+                    await _paymentRepository.SaveChangesAsync();
+
+                    package.Id = 10;
+                    package.Payment = payment;
+                    package.DeliveryDate = DateTime.UtcNow.AddDays(4);
+
+                    if (defaultSorting == null)
+                    {
+                        throw new ArgumentException(nameof(defaultSorting));
+                    }
+
+                    package.Sorting = defaultSorting;
+
+                    await _packageRepository.AddAsync(package);
+                    await _packageRepository.SaveChangesAsync();
+
+                    transaction.Commit();
+
+                    return ResponseFactory.CreateSuccessResponse<RegisterPackageResponse>();
+                } catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
             }
-
-            var payment = _mapper.Map<Payment>(request.Payment);
-            payment.Status = PaymentStatus.InProgress;
-            await _paymentRepository.AddAsync(payment);
-            
-            package.Id = 10;
-            package.Payment = payment;
-            package.DeliveryDate = DateTime.UtcNow.AddDays(4);
-
-            package.Sorting = defaultSorting;
-            
-            await _packageRepository.AddAsync(package);
-            await _packageRepository.SaveChangesAsync();
-            
-            return ResponseFactory.CreateSuccessResponse<RegisterPackageResponse>();
         }
 
         private bool AddressesAreEqual(Address address, AddressDto requestDeliveryAddress)
