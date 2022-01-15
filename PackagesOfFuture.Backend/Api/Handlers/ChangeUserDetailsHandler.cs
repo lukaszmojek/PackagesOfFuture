6ccadd83b953051b65b61c16a944ca9c23d9028a
lukaszmojek <lukasz.mojek@gmail.com>
2022-01-15 13:14:52 +0100

    Update ChangeUserDetailsHandler and add tests for it

--- PackagesOfFuture.Backend/Api/Handlers/ChangeUserDetailsHandler.cs
+++ PackagesOfFuture.Backend/Api/Handlers/ChangeUserDetailsHandler.cs
@@ -7,6 +7,7 @@ using Contracts.Responses;
 using Data.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using ResourceEnums;
 
 namespace Api.Handlers;
 
@@ -30,20 +31,42 @@ public class
                            .SingleAsync(x => x.Id == command.Id, cancellationToken)
                        ?? throw new Exception("User does not exist");
 
+            if (!command.RequestingUser.Id.Equals(user.Id) && command.RequestingUser.Type != UserType.Administrator)
+            {
+                var message =
+                    $"User with id {command.RequestingUser.Id} tried to edit user with id {user.Id} as an not administrator user";
+                throw new UnauthorizedAccessException(message);
+            }
+
             user.FirstName = command.FirstName;
             user.LastName = command.LastName;
-            user.Type = command.Type;
 
             user.Address.City = command.Address.City;
             user.Address.Street = command.Address.Street;
             user.Address.HouseAndFlatNumber = command.Address.HouseAndFlatNumber;
             user.Address.PostalCode = command.Address.PostalCode;
 
+            if (user.Type != command.Type)
+            {
+                if (command.RequestingUser.Type != UserType.Administrator)
+                {
+                    var message =
+                        $"User with id {command.RequestingUser.Id} tried to change type of the user with id {user.Id} as an not administrator user";
+                    throw new UnauthorizedAccessException(message);
+                }
+
+                user.Type = command.Type;
+            }
+
             await _dbContext.SaveChangesAsync(cancellationToken);
 
             return ResponseFactory.CreateSuccessResponse<ChangeUserDetailsResponse>();
         }
-        catch (Exception)
+        catch (UnauthorizedAccessException e)
+        {
+            return ResponseFactory.CreateFailureResponse<ChangeUserDetailsResponse>(e.Message);
+        }
+        catch (Exception e)
         {
             return ResponseFactory.CreateFailureResponse<ChangeUserDetailsResponse>();
         }
---
+++ PackagesOfFuture.Backend/HandlersTests/HandlerTestBase.cs
@@ -0,0 +1,6 @@
+﻿namespace Tests;
+
+public abstract class HandlerTestBase
+{
+    public string DatabaseConnectionString = "User ID=postgres;Password=Password12!;Host=localhost;Port=5432;Database=PackagesOfFuture;Pooling=true";
+}
